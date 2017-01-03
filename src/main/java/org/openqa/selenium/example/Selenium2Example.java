218ba115578056183bff8a96f3c91f0028f3673b
Mark Derricutt <mark.derricutt@smxemail.com>
2017-01-03 20:03:39 +1300

    Moved source file to correct location

--- src/main/java/org/openqa/selenium/example/Selenium2Example.java
+++ src/main/java/org/openqa/selenium/example/Selenium2Example.java
@@ -3,7 +3,6 @@ package org.openqa.selenium.example;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.phantomjs.PhantomJSDriver;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.WebDriverWait;
